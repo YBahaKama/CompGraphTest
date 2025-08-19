ba3dd76c19bdd0f4880f5c1035b66ee56bf5238f
Yusuf Baha Kama <m_ybaha@hotmail.com>
2025-08-19 11:41:10 +0300

    some other fixes 20

--- cmake/install-rules.cmake
+++ cmake/install-rules.cmake
@@ -1,6 +1,21 @@
-install(
-    TARGETS executable_exe
-    RUNTIME COMPONENT executable_Runtime
+# Install the ComputerGraphics library
+install(TARGETS ComputerGraphics
+    EXPORT ComputerGraphicsTargets
+    ARCHIVE DESTINATION lib
+    LIBRARY DESTINATION lib
+    RUNTIME DESTINATION bin
+)
+
+# Install public headers
+install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/
+    DESTINATION include
+)
+
+# Export targets for find_package support
+install(EXPORT ComputerGraphicsTargets
+    FILE ComputerGraphicsTargets.cmake
+    NAMESPACE ComputerGraphics::
+    DESTINATION lib/cmake/ComputerGraphics
 )
 
 if(PROJECT_IS_TOP_LEVEL)
