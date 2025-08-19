9beeb65bde0d83faa49129a7c95bfd4a9574defe
Yusuf Baha Kama <m_ybaha@hotmail.com>
2025-08-19 12:33:32 +0300

    21

--- cmake/dev-mode.cmake
+++ cmake/dev-mode.cmake
@@ -1,31 +1,25 @@
-include(cmake/folders.cmake)
+    include(cmake/folders.cmake)
 
-include(CTest)
-if(BUILD_TESTING)
-  add_subdirectory(test)
-endif()
+    include(CTest)
+    if(BUILD_TESTING)
+      add_subdirectory(test)
+    endif()
 
-add_custom_target(
-    run-exe
-    COMMAND executable_exe
-    VERBATIM
-)
-if(TARGET executable_exe)
-  add_dependencies(run-exe executable_exe)
-endif()
+    add_custom_target(
+        run-exe
+        COMMAND executable_exe
+        VERBATIM
+    )
+    if(TARGET executable_exe)
+      add_dependencies(run-exe executable_exe)
+    endif()
 
+    option(ENABLE_COVERAGE "Enable coverage support separate from CTest's" OFF)
+    if(ENABLE_COVERAGE)
+      include(cmake/coverage.cmake)
+    endif()
 
-option(BUILD_MCSS_DOCS "Build documentation using Doxygen and m.css" OFF)
-if(BUILD_MCSS_DOCS)
-  include(cmake/docs.cmake)
-endif()
+    include(cmake/lint-targets.cmake)
+    include(cmake/spell-targets.cmake)
 
-option(ENABLE_COVERAGE "Enable coverage support separate from CTest's" OFF)
-if(ENABLE_COVERAGE)
-  include(cmake/coverage.cmake)
-endif()
-
-include(cmake/lint-targets.cmake)
-include(cmake/spell-targets.cmake)
-
-add_folders(Project)
+    add_folders(Project)
